fe79c4fa634c2c6624cc252c38920b36468d0c0f
Tomas Poveda <tpovedatd@gmail.com>
2020-10-30 21:06:58 +0100

    Added core unit test library implementation

---
+++ tpDcc/libs/unittests/core/consts.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains tpDcc-libs-unittest constant definitions
+"""
+
+from __future__ import print_function, division, absolute_import
+
+
+UNIT_TEST_VAR = 'TPDCC_UNIT_TEST'
